9b977f03c7d8992a89a00196943fbe92472aa23e
mihaihe1 <hernest001@gmail.com>
2021-07-05 12:13:04 +0300

    update tema3

--- Tema3/tema3.py
+++ Tema3/tema3.py
@@ -1,37 +1,40 @@
-def sum_1(*args, **kwargs):
+def sum_args(*args, **kwargs):
     sum = 0
     for x in args:
+        if isinstance(x, (int, float)):
+            sum += x
+    for x in kwargs.values():
         if isinstance(x, (int, float)):
             sum += x
 
     return sum
 
 
-def sum_2(n):
+def sum_n(n):
     if n == 0:
         return 0
 
-    return n + sum_2(n-1)
+    return n + sum_n(n-1)
 
 
-def sum_3(n):
+def sum_even(n):
     if n == 0:
         return 0
 
     if n % 2 == 1:
-        return sum_3(n-1)
+        return sum_even(n-1)
 
-    return n + sum_3(n-2)
+    return n + sum_even(n-2)
 
 
-def sum_4(n):
+def sum_odd(n):
     if n < 0:
         return 0
 
     if n % 2 == 0:
-        return sum_4(n-1)
+        return sum_odd(n-1)
 
-    return n + sum_4(n-2)
+    return n + sum_odd(n-2)
 
 
 def is_int():
@@ -44,16 +47,16 @@ def is_int():
         return x
 
 
-print(sum_1(1, 5, -3, 'abc', [12, 56, 'cad']))
-print(sum_1())
-print(sum_1(2, 4, 'abc', param_1=2))
+print(sum_args(1, 5, -3, 'abc', [12, 56, 'cad']))
+print(sum_args())
+print(sum_args(2, 4, 'abc', param_1=2))
 
-print(sum_2(8))
+print(sum_n(8))
 
-print(sum_3(6))
-print(sum_3(7))
+print(sum_even(6))
+print(sum_even(7))
 
-print(sum_4(7))
-print(sum_4(8))
+print(sum_odd(7))
+print(sum_odd(8))
 
 print(is_int())
